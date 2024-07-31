146ba7dea9c9ce9e11369078ee2ba4a521e3f1e4
Cidjohn <cidjohn31@gmail.com>
2024-07-31 14:18:04 +0800

    api call for auth and content

--- src/helper/database/restAPI.jsx
+++ src/helper/database/restAPI.jsx
@@ -1,9 +1,10 @@
 const restAPI = () => {
   const apiUrl2 = process.env.REACT_APP_API_SERVER;
-  const apiAuth2 = process.env.REACT_APP_API_AUTH_SERVER;
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const apiAuth = process.env.REACT_APP_API_AUTH;
   const API_CALL = {
-    host: apiUrl2,
-    auth: apiAuth2,
+    host: apiUrl2 + "/content",
+    auth: apiUrl2 + "/auth",
   };
   return API_CALL;
 };
