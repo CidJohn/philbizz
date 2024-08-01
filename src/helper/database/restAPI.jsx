e9de907b010308209c465fee0b8ced09aaab9330
Cidjohn <cidjohn31@gmail.com>
2024-08-01 12:45:41 +0800

    features/login/implementationV2

--- src/helper/database/restAPI.jsx
+++ src/helper/database/restAPI.jsx
@@ -2,8 +2,8 @@ const restAPI = () => {
   const apiUrl2 = process.env.REACT_APP_API_SERVER;
   const apiUrl = process.env.REACT_APP_API_URL;
   const API_CALL = {
-    host: apiUrl + "/content",
-    auth: apiUrl + "/auth",
+    host: apiUrl2 + "/content",
+    auth: apiUrl2 + "/auth",
   };
   return API_CALL;
 };
