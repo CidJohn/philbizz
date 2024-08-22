d7d1349244a001e7704788d86db99a050fd8f001
CidJohn <97021610+CidJohn@users.noreply.github.com>
2024-08-22 10:31:21 +0800

    feature/mainpage/socialmediaUI

--- src/content/cardContent.jsx
+++ src/content/cardContent.jsx
@@ -17,6 +17,21 @@ const cardContent = [
       "https://media.vaticannews.va/media/content/dam-archive/vaticannews/agenzie/images/afp/2019/01/09/01/philippines-religion-black-nazarene-1546992527761.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.750.422.jpeg",
     desc: "Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.",
   },
+  {
+    socialmedia: "Facebook",
+    imageURL:
+      "https://logos-world.net/wp-content/uploads/2020/05/Facebook-Logo.png",
+  },
+  {
+    socialmedia: "Youtube",
+    imageURL:
+      "https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png",
+  },
+  {
+    socialmedia: "Instagram",
+    imageURL:
+      "https://1000logos.net/wp-content/uploads/2017/02/ig-logo-768x256.png",
+  },
 ];
 
 export default cardContent;
