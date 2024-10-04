0fac34f947d9f9665a60354e925898a0e682abe5
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-10-04 10:48:49 +0800

    feature/sp-1-86eqdu9w7/Main-Page-List-UI

--- src/components/Listedcard/Listedcard.jsx
+++ src/components/Listedcard/Listedcard.jsx
@@ -22,31 +22,54 @@ const Listedcard = (props) => {
   };
 
   return (
-    <div className=" ">
-      <section id={section} className="">
-        <div className="flex   md:mt-1">
-          <h1 className="text-4xl  mx-2 font-bold">{t(title)}</h1>
-        </div>
-        <div className=" ">
-          {listItems
-            ? listItems.map((item, index) => (
-                <div key={index} className=" border-b  ">
-                  <List
-                    onLink={() => handleLink(item.title)}
-                    title={item.title}
-                    desc={item.description}
-                    image={item.images || item.image}
-                    datetime={item.created_at}
-                    imgstyle={{ width: "10vw", height: "10vh" }}
-                    className={
-                      "w-full border-none  shadow-none hover:bg-slate-100 "
-                    }
+    <section id={section} className="">
+      <div className="flex   md:mt-1">
+        <h1 className="text-4xl  mx-2 font-bold">{t(title)}</h1>
+      </div>
+      <div className="w-full gap-6 mr-12">
+        {listItems
+          ? listItems.map((item, index) => (
+              <div
+                key={index}
+                className="w-full h-auto mb-6 border border-[#013A63]/15 rounded-md p-4"
+              >
+                <article className="group overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm ">
+                  <img
+                    alt=""
+                    src={item.images || item.image}
+                    className="h-56 w-full rounded-xl object-cover "
                   />
-                </div>
-              ))
-            : ""}
-        </div>
-        {/* <div className="mt-5 text-center">
+                  <div className="p-8 bg-[#013A63]/5">
+                    <div className="flex items-center justify-between ">
+                      <h3 className="text-lg font-medium text-[#013A63] fira-sans-bold">
+                        {item.title}
+                      </h3>
+                      <span class="whitespace-nowrap rounded-full bg-[#013A63]/5 px-3.5 py-1.5 text-xs text-[#013A63]">
+                        <Dateformat dateString={item.created_at} />
+                      </span>
+                    </div>
+                    <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 fira-sans-condensed-regular">
+                      {item.description}
+                    </p>
+                    <a
+                      className="group mt-4 inline-flex items-center gap-1 text-sm text-[#013A63] fira-sans-condensed-bold"
+                      href={item.title}
+                    >
+                      Find out more
+                      <span
+                        aria-hidden="true"
+                        className="block transition-all group-hover:ms-0.5 rtl:rotate-180"
+                      >
+                        &rarr;
+                      </span>
+                    </a>
+                  </div>
+                </article>
+              </div>
+            ))
+          : ""}
+      </div>
+      {/* <div className="mt-5 text-center">
           <a
             href={`/${links}`}
             className=" p-2  text-gray-700 border  hover:bg-blue-500 hover:text-gray-100 rounded-lg"
@@ -54,8 +77,7 @@ const Listedcard = (props) => {
             View More {`>>`}
           </a>
         </div> */}
-      </section>
-    </div>
+    </section>
   );
 };
 
