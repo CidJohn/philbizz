7312a4681633053579e8eb6cc6f7c146b19ae6bd
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-11-07 15:12:20 +0800

    feature/sp3-86eqpcq89/Content-Page-Responsiveness

--- src/pages/Selection/Description/Description.jsx
+++ src/pages/Selection/Description/Description.jsx
@@ -12,46 +12,41 @@ const Description = (props) => {
         ? content.map(
             (item, index) =>
               item.header === pageName && (
-                <div className="" key={index}>
-                  <div className=" flex mt-5">
-                    <div className="flex flex-col ">
+                <div className='' key={index}>
+                  <div className=' flex mt-5'>
+                    <div className='flex flex-col '>
                       <h1
-                        className="text-4xl md:text-6xl mb-4 fira-sans-bold "
+                        className='text-4xl md:text-6xl mb-4 fira-sans-bold '
                         style={{ color: txtHeaderColor }}
                       >
                         {t(item.header)}
                       </h1>
-                      <p className="text-left mb-2 text-lg text-slate-500 fira-sans-condensed-regular ">
+                      <p className='text-left mb-2 text-sm md:text-md lg:text-lg text-slate-500 fira-sans-condensed-regular '>
                         {item.paragraph}
                       </p>
                     </div>
                     {item.image && (
-                      <figure className="max-w-md">
-                        <Images
-                          className="rounded-lg"
-                          src={item.image}
-                        />
-                        <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
+                      <figure className='w-full'>
+                        <Images className='rounded-lg' src={item.image} />
+                        <figcaption className='mt-2 text-sm text-center text-gray-500 dark:text-gray-400'>
                           {t(item.header)}
                         </figcaption>
                       </figure>
                     )}
                   </div>
-                  <div className="grid grid-cols-2 ">
+                  <div className='w-full lg:grid lg:grid-cols-2 '>
                     <div
                       className={
-                        carousel
-                          ? "flex flex-col "
-                          : "flex flex-col max-w-[30vw] "
+                        carousel ? "flex flex-col " : "flex flex-col w-full "
                       }
                     >
-                      <p className="text-left text-gray-400  font-sans ">
+                      <p className='text-left text-gray-400 text-sm lg:text-md fira-sans-condensed-regular w-full '>
                         {item.description}
                       </p>
                     </div>
                     {carousel && (
                       <>
-                        <div className="p-2 border">
+                        <div className='p-2 border'>
                           <Imagecarousel images={carousel} />
                         </div>
                       </>
--- src/utils/Selection/ContentLayout.jsx
+++ src/utils/Selection/ContentLayout.jsx
@@ -27,62 +27,75 @@ const ContentLayout = ({
   currentRef,
 }) => {
   return (
-    <div className=" mx-auto flex  w-[75vw] justify-center gap-3 ">
+    <div className='flex flex-col px-6 lg:flex-row w-full lg:px-40 justify-center gap-3 mt-4 '>
       <SonnerToaster />
-
-      <div className=" mt-2">{renderTreeView()}</div>
-      <div className="flex flex-col">
-        <div className="flex flex-col ">
-          <div className="min-w-[45vw]">
+      <div className=''>{renderTreeView()}</div>
+      <div className='w-full flex flex-col'>
+        <div className='flex flex-col w-full '>
+          <div className='w-full'>
             <Description
               content={desc}
               pageName={adName}
               txtHeaderColor={sideBarColor ? sideBarColor.textColor : "#E63946"}
             />
           </div>
-          <section id="cards" className="sticky top-5 " ref={currentRef}>
+          <section id='cards' className='sticky top-5 ' ref={currentRef}>
             <div
-              className="flex flex-col  mt-5 p-2"
-              style={{ backgroundColor: sideBarColor ? sideBarColor.bgColor : "#E639460D" }}
+              className='flex flex-col  mt-5 p-2'
+              style={{
+                backgroundColor: sideBarColor
+                  ? sideBarColor.bgColor
+                  : "#E639460D",
+              }}
             >
-              <div className="flex flex-col lg:flex-row items-center justify-center  ">
-                <div className="flex flex-col max-w-80">
-                  <div className="text-md">Address:</div>
+              <div className='flex flex-col lg:flex-row items-center justify-center  '>
+                <div className='flex flex-col w-full lg:max-w-80'>
+                  <div className='text-md'>Address:</div>
                   <Dropdown
-                    name="category"
+                    name='category'
                     value={dropdownValue}
                     onChange={handleDropdownChange}
                     options={dropdownOptions}
                     placeholder={"Select All"}
-                    width="300px"
-                    selectWidth="500px"
-                    adsBorder={sideBarColor ? sideBarColor.textColor: "#E63946"}
-                    textColor={sideBarColor ? sideBarColor.textColor: "#E63946"}
+                    width='300px'
+                    selectWidth='500px'
+                    adsBorder={
+                      sideBarColor ? sideBarColor.textColor : "#E63946"
+                    }
+                    textColor={
+                      sideBarColor ? sideBarColor.textColor : "#E63946"
+                    }
                   />
                 </div>
-                <div className=" hidden lg:block text-sm py-5  h-[30px] border-gray-500 mx-3 ">
-                  <div className="flex font-black text-sm text-gray-400 ">
+                <div className=' hidden lg:block text-sm py-5  h-[30px] border-gray-500 mx-3 '>
+                  <div className='flex font-black text-sm text-gray-400 '>
                     or
                   </div>
                 </div>
-                <div className="flex flex-col mt-5 lg:mt-0">
-                  <div className="text-md">Branch Name:</div>
+                <div className='flex flex-col mt-5 lg:mt-0 w-full'>
+                  <div className='text-md'>Branch Name:</div>
                   <SearchBar
                     hidden={true}
                     onSearch={handleOnSearch}
-                    adsBorder={sideBarColor ? sideBarColor.textColor: "#E63946"}
-                    textColor={sideBarColor ? sideBarColor.textColor: "#E63946"}
-                    placeholderColor={sideBarColor ? sideBarColor.textColor: "#E63946"}
+                    adsBorder={
+                      sideBarColor ? sideBarColor.textColor : "#E63946"
+                    }
+                    textColor={
+                      sideBarColor ? sideBarColor.textColor : "#E63946"
+                    }
+                    placeholderColor={
+                      sideBarColor ? sideBarColor.textColor : "#E63946"
+                    }
                   />
                 </div>
               </div>
-              <div className=" mt-3 grid grid-cols-3  gap-2 ">
+              <div className='w-full flex flex-col mt-3 lg:grid lg:grid-cols-3  ap-2 '>
                 {handleCards(currentItems)}
               </div>
             </div>
           </section>
         </div>
-        <div className="mt-5 grid justify-items-end">
+        <div className='w-full mt-5 grid justify-items-end'>
           {!selectedItem?.id ? (
             filterData.length === 0 ? (
               <Pagination
@@ -99,40 +112,48 @@ const ContentLayout = ({
           )}
         </div>
       </div>
-      <div className="flex  hidden lg:block mt-2 ">
-        <div className="sticky top-5">
-          <div className="px-3">
+      <div className='w-full lg:w-[30rem] lg:block mt-2 '>
+        <div className='flex flex-col w-full lg:sticky top-5'>
+          <div className='w-full px-3'>
             <h1
               className={`font-sans font-bold border text-center  rounded-lg p-1`}
               style={{
-                color: sideBarColor ? sideBarColor.textColor: "#E63946",
-                borderColor: sideBarColor ? sideBarColor.textColor: "#E63946",
+                color: sideBarColor ? sideBarColor.textColor : "#E63946",
+                borderColor: sideBarColor ? sideBarColor.textColor : "#E63946",
               }}
             >
               {adName} Latest Ads
             </h1>
           </div>
-          <div className="flex flex-col gap-2 p-2">
+          <div className='w-full flex flex-col gap-2 p-2'>
             {sideAds.map((item, index) => (
               <div
-                className="border-b-2 dashed py-3 border-dashed"
-                style={{ borderColor: sideBarColor ? sideBarColor.textColor: "#E63946" }}
+                className='border-b-2 dashed py-3 border-dashed'
+                style={{
+                  borderColor: sideBarColor
+                    ? sideBarColor.textColor
+                    : "#E63946",
+                }}
                 key={index}
               >
-                <div className="bg-cover mx-auto ">
+                <div className='w-full  '>
                   <Card
                     src={item.card_image}
                     title={item.title}
                     desc={item.description}
                     style={{
-                      width: "230px",
+                      width: "100%",
                       backgroundSize: "cover",
                       border: "1px solid",
-                      borderColor: sideBarColor ? sideBarColor.textColor: "#E63946",
+                      borderColor: sideBarColor
+                        ? sideBarColor.textColor
+                        : "#E63946",
                     }}
                     onLink={() => handleLink(item.title)}
                     btnColor={sideBarColor ? sideBarColor.bgColor : "#E639460D"}
-                    textColor={sideBarColor ? sideBarColor.textColor: "#E63946"}
+                    textColor={
+                      sideBarColor ? sideBarColor.textColor : "#E63946"
+                    }
                   />
                 </div>
               </div>
