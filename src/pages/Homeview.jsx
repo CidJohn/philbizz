8dbca5538b77d24629e01e42707b9b6697add0fd
Cidjohn <cidjohn31@gmail.com>
2024-11-11 13:24:59 +0800

    feature/sp3-86eqpvp5c/Main_page_list_of_Content_refactor

--- src/pages/Homeview.jsx
+++ src/pages/Homeview.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Businessview from "./Homeview/Businessview/Businessview";
 import { HeroBanner } from "./Homeview/HeroBanner/HeroBanner";
 
 function Homeview({ data }) {
   const { t } = useTranslation();
 
   return (
-    <div className='w-full'>
-      <HeroBanner blogData={data.blogData} />
-      {/* <Herobanners /> */}
-      <Businessview
+    <div className="w-full">
+      <HeroBanner
+        blogData={data.blogData}
         navbar={data.navbar}
         businessCarousel={data.businessSettings}
       />
+   
     </div>
   );
 }
