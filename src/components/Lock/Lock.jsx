0a96073306d038c2302245c646128e3310544955
Cidjohn <cidjohn31@gmail.com>
2024-11-15 13:56:53 +0800

    feature/sp3-86eqr9qff/Card_Setting_API_Adjustment

--- src/components/Lock/Lock.jsx
+++ src/components/Lock/Lock.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-
+import React from "react";
 const Lock = () => {
   return (
-    <div id="container_anim" className="flex flex-col items-center">
+    <div id="container_anim body_lock" className="flex flex-col items-center">
       <div id="lock" className="key-container">
         <svg
           xmlns="http://www.w3.org/2000/svg"
