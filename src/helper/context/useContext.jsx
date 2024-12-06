f0449c0362dedd9c7e39dbfc06c49db29c379a1b
CidJohn <97021610+CidJohn@users.noreply.github.com>
2024-12-06 17:14:06 +0800

    adjust api call in mainpage content list

--- src/helper/context/useContext.jsx
+++ src/helper/context/useContext.jsx
@@ -2,27 +2,41 @@ import { createContext, useContext, useEffect, useState } from "react";
 import {
   useContentView,
   useContentViewList,
+  useMainPageContentList,
   useViewContentInfo,
 } from "../database/useCardSettings";
 
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  const [contentList, setContentList] = useState([]);
-  const [contentInfo, setContentInfo] = useState([]);
   const { viewContent, setHeader } = useContentViewList();
   const { setUuid, viewInfo, infoLoader } = useViewContentInfo();
+  const { mainList, listLoader } = useMainPageContentList();
+  const [contentList, setContentList] = useState([]);
+  const [contentInfo, setContentInfo] = useState([]);
+  const [mainContentList, setMainContentList] = useState([]);
 
   useEffect(() => {
     setContentList(viewContent ? viewContent : []);
     if (viewInfo.length > 0) {
       viewInfo.map((item) => setContentInfo(item));
     }
-  }, [viewContent, viewInfo]);
+    console.log(mainList);
+    if (mainList.length > 0) {
+      setMainContentList(mainList);
+    }
+  }, [viewContent, viewInfo, mainList]);
 
   return (
     <GlobalContext.Provider
-      value={{ contentList, setHeader, setUuid, contentInfo, infoLoader }}
+      value={{
+        contentList,
+        setHeader,
+        setUuid,
+        contentInfo,
+        infoLoader,
+        mainContentList,
+      }}
     >
       {children}
     </GlobalContext.Provider>
